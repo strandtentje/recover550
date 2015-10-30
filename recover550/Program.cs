3a75e419c3e31cb426cc0bf84532a647266dced4
Rob Tierolff <teh@borreh.nl>
2015-10-30 13:25:32 +0100

    Started work on recoverytool.

---
+++ recover550/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+
+namespace recover550
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Stream stdin = Console.OpenStandardInput;
+
+
+		}
+	}
+}
